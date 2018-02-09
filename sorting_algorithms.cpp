84e0b5621900a954609bbf8098c58da6251da3d4
pilouihtl <31956991+pilouihtl@users.noreply.github.com>
2018-02-09 13:15:55 +0100

    Add files via upload

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -10,3 +10,37 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include <stdio.h>
+#include <stdbool.h>
+#include "sorting_algorithms.h"
+#include <stdlib.h>
+
+
+void init_random(int* small_array, int SMALL)
+{
+  for(int i = 0; i < SMALL; i++){
+    small_array[i] = rand();
+  }
+}
+void bubble_sort(int* small_array, int SMALL){
+  for(int j = 0; j < SMALL; j++){
+    for(int i = j; i < SMALL; i++){
+      if(small_array[j] > small_array[i]){
+        int swap = small_array[j];
+        small_array[j] = small_array[i];
+        small_array[i] = swap;
+      }
+    }
+  }
+}
+void insertion_sort(int* small_array, int SMALL){
+    for (int j = 1; j < SMALL; j++) {
+      int key = small_array[j];
+      int i = j - 1;
+      while (i >= 0 && small_array[i] > key) {
+        small_array[i + 1] = small_array[i];
+        i--;
+      }
+      small_array[i + 1] = key;
+    }
+}
--- stopwatch.cpp
+++ stopwatch.cpp
@@ -10,3 +10,23 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include <stdio.h>
+#include <stdbool.h>
+#include "stopwatch.h"
+#include <time.h>
+
+clock_t start, end = 0;
+void start_stopwatch(){
+  start = clock();
+  end = 0;
+}
+void stop_stopwatch(){
+  end = clock();
+
+}
+double elapsed_time(){
+  if(end == 0){
+      stop_stopwatch();
+  }
+  return double(end - start) / CLOCKS_PER_SEC * 1000;
+}
